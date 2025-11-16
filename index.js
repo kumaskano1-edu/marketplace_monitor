2ba32f7b5b0e63b01b2c3bfe592299a2b51def03
Kurmanbek Karaev <57332034+kumaskano1-edu@users.noreply.github.com>
2025-11-15 21:18:36 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const TelegramBot = require("node-telegram-bot-api");
 const { Redis } = require("@upstash/redis");
 
 // ------------------ CONFIG ---------------------
-const BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN";
+const BOT_TOKEN = "8303035400:AAG4I6ScEoJucL06TZ_e5bLdARj5n1brHng";
 const CHAT_ID = "5332581775";
 
 const bot = new TelegramBot(BOT_TOKEN, { polling: true });
